fad5df59cde35ebab461c08a76c704e994d4cd99
xeyzo <schneidergw@yahoo.com>
2020-08-26 01:48:08 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="row" style="margin-top: 100px;">
+      <div class="col l9">
+        <productlist v-for="product in products" :key="product.id" :id="product.id" :name="product.name" :image="product.image" :price="product.price" />
+      </div>
+      <div class="col l3">
+        <shoppingcart />
+      </div>
+    </div>
+
+   
+
+    
+      
+  </div>
+</template>
+
+<script>
+import productlist from './productlist';
+import shoppingcart from './shoppingcart';
+
+
+export default {
+  name: 'app',
+  computed: {
+    products() { return this.$store.getters.products; }
+  },
+  components: {
+    productlist,
+    shoppingcart,
+  }
+}
+</script>
+
+<style>
+  .cartIcon {
+    padding: 5px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import '../node_modules/materialize-css/dist/css/materialize.min.css'
+import '../node_modules/materialize-css/dist/js/materialize.min.js'
+import './assets/jquery.js'
+import store from './store'
+
+require('../node_modules/vue-ionicons/ionicons.css')
+
+
+new Vue({
+  el: '#app',
+  store,
+  render: h => h(App)
+})
